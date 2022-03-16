e41bb49ec15c93b19eaa9d6af357ae0b0842a91c
LoboTrya3 <con.zrylov@gmail.com>
2022-03-16 19:45:33 +0300

    Ball Class File (16.03.2022)

--- ball.py
+++ ball.py
@@ -4,8 +4,8 @@ class Ball:
     def __init__(
         self,
         window,
-        velocity_x=4,
-        velocity_y=-4
+        velocity_x=6,
+        velocity_y=-6
     ):
         self.image = pygame.Surface((15, 15))
         self.image.fill((255, 255, 255))
@@ -20,7 +20,7 @@ class Ball:
     def draw(self):
         self.window.blit(self.image, self.rect)
 
-    def movement(self):
+    def movement(self, player, opponent):
         self.rect.x += self.velocity_x
         self.rect.y += self.velocity_y
         if self.rect.right >= self.window_rect.right:
@@ -30,4 +30,11 @@ class Ball:
         if self.rect.top <= self.window_rect.top:
             self.velocity_y *= -1
         if self.rect.bottom >= self.window_rect.bottom:
+            self.velocity_y *= -1
+
+        if self.rect.colliderect(player.rect):
+            self.velocity_x *= -1
+            self.velocity_y *= -1
+        if self.rect.colliderect(opponent.rect):
+            self.velocity_x *= -1
             self.velocity_y *= -1
