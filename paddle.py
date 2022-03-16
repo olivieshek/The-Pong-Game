3509cdc4c9eacabd0634194e8f613718fcac5d61
LoboTrya3 <con.zrylov@gmail.com>
2022-03-16 19:04:42 +0300

    16.03.2022

--- paddle.py
+++ paddle.py
@@ -1,14 +1,41 @@
+from curses import KEY_UP
 import pygame
 
 class Paddle:
-    def __init__(self, window, x_pos, objImage):
-        self.image = objImage
+    def __init__(
+        self,
+        window,
+        isAuto=False,
+        color=(255, 255, 255),
+        key_up=pygame.K_UP,
+        key_down=pygame.K_DOWN,
+        velocity=10
+    ):
+        self.image = pygame.Surface((10, 100))
+        self.image.fill(color)
         self.rect = self.image.get_rect()
         self.window = window
         self.window_rect = self.window.get_rect()
-        self.rect.centerx = x_pos
+        if isAuto is True:
+            self.rect.centerx = window.get_width() - window.get_width() / 30 - self.image.get_width() / 2
+        else:
+            self.rect.centerx = window.get_width() / 30 + self.image.get_width() / 2
         self.rect.centery = self.window.get_height() / 2
-        self.speed = 20
+        # if isAuto == True:
+        #     print('auto')
+        self.key_up = key_up
+        self.key_down = key_down
+        self.velocity = velocity
+        self.isAuto = isAuto
 
     def draw(self):
         self.window.blit(self.image, self.rect)
+
+    def controls(self, keys):
+        if self.isAuto:
+            pass
+        else:
+            if keys[self.key_up]:
+                self.rect.y -= self.velocity
+            if keys[self.key_down]:
+                self.rect.y += self.velocity
