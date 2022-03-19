31fae610499432a5b4a4808fd13dee00ec40495c
LoboTrya3 <con.zrylov@gmail.com>
2022-03-19 15:44:02 +0300

    Paddle Class File

--- paddle.py
+++ paddle.py
@@ -9,7 +9,8 @@ class Paddle:
         color=(255, 255, 255),
         key_up=pygame.K_UP,
         key_down=pygame.K_DOWN,
-        velocity=10
+        velocity=10,
+        score=0
     ):
         self.image = pygame.Surface((10, 100))
         self.image.fill(color)
@@ -21,24 +22,23 @@ class Paddle:
         else:
             self.rect.centerx = window.get_width() / 30 + self.image.get_width() / 2
         self.rect.centery = self.window.get_height() / 2
-        # if isAuto == True:
-        #     print('auto')
         self.key_up = key_up
         self.key_down = key_down
         self.velocity = velocity
         self.isAuto = isAuto
+        self.score = score
 
     def draw(self):
         self.window.blit(self.image, self.rect)
 
     def controls(self, keys, ball):
         if self.isAuto:
-            if self.rect.y < ball.rect.y:
+            if self.rect.y < ball.rect.y and self.rect.bottom < self.window_rect.bottom:
                 self.rect.y += self.velocity
-            else:
+            if self.rect.y > ball.rect.y and self.rect.top > self.window_rect.top:
                 self.rect.y -= self.velocity
-        else:
-            if keys[self.key_up]:
+        if not self.isAuto:
+            if keys[self.key_up] and self.rect.top > self.window_rect.top:
                 self.rect.y -= self.velocity
-            if keys[self.key_down]:
+            if keys[self.key_down] and self.rect.bottom < self.window_rect.bottom:
                 self.rect.y += self.velocity
