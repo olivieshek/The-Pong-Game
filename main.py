b03bbc9ecdb168cfb5504cd6ebb2972087c4d8fc
LoboTrya3 <con.zrylov@gmail.com>
2022-03-19 15:43:55 +0300

    Main File

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import pygame
-from PIL import Image
 from paddle import Paddle
 from ball import Ball
+from score import Score
 
 pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.set_volume(1)
+pygame.mixer.music.load('assets/sound.mp3')
 window = pygame.display.set_mode((800, 600))
+window_rect = window.get_rect()
 clock = pygame.time.Clock()
 pygame.display.set_caption('The Pong Game')
-background = pygame.image.load('assets/background.png')
+# background = pygame.image.load('assets/background.png')
 player = Paddle(window)
 opponent = Paddle(window, isAuto=True)
 ball = Ball(window)
+player_score = Score(
+    window,
+    pos_x=window_rect.center[0] - 48,
+    pos_y=window_rect.midtop[1] + 48,
+    owner=player
+)
+opponent_score = Score(
+    window,
+    pos_x=window_rect.center[0] + 48,
+    pos_y=window_rect.midtop[1] + 48,
+    owner=opponent
+)
 
 running = True
 while running:
     window.fill((0, 0, 0))
-    window.blit(background, (0, 0))
+    # window.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -24,8 +40,10 @@ while running:
 
     player.draw()
     player.controls(keys, ball)
+    player_score.update()
     opponent.draw()
     opponent.controls(keys, ball)
+    opponent_score.update()
     ball.draw()
     ball.movement(player, opponent)
     pygame.display.flip()
